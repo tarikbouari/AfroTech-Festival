da286d5af6c62802ac24b363835f9c918a0aee3c
Rikstone <tarikbouari@gmail.com>
2022-08-28 12:36:49 -0700

    update menu

--- js/menu.js
+++ js/menu.js
@@ -1,23 +1,19 @@
-let menu = document.querySelectorAll('.bar');
+let menu = document.querySelectorAll(".bar");
 
+const navList = document.getElementById("nav-list");
+const closeBtn = document.getElementById("close");
 
-const navList = document.getElementById('nav-list');
-const closeBtn = document.getElementById('close');
-
-menu.forEach( (menuBtn) => {
-  
-  menuBtn.addEventListener('click', () => {
-    navList.classList.add('active-navlist');
-    closeBtn.classList.add('close-active');
-    closeBtn.style.visibility = 'visible';
+menu.forEach((menuBtn) => {
+  menuBtn.addEventListener("click", () => {
+    navList.classList.add("active-navlist");
+    closeBtn.classList.add("close-active");
+    closeBtn.style.visibility = "visible";
   });
 });
 
-
-closeBtn.addEventListener('click', () => {
-  // navList.style.transform ='translateX(-100%)';
-  closeBtn.style.visibility = 'hidden';
-  closeBtn.classList.remove('close-active');
-   window.location.reload();
-
+closeBtn.addEventListener("click", () => {
+  navList.style.transform = "translateX(-100%)";
+  closeBtn.style.visibility = "hidden";
+  closeBtn.classList.remove("close-active");
+  window.location.reload();
 });
